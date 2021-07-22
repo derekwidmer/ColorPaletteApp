bf9cd5ab171af4f5f6e7cc402231c1d3e1d87d7f
Derek Widmer <dwidmer99@gmail.com>
2021-07-21 22:16:02 -0400

    Added NewPaletteForm and Link on homepage to it

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { generatePalette } from './colorHelpers'
 import { Route, Switch } from 'react-router-dom'
 import PaletteList from './PaletteList'
 import SingleColorPalette from './SingleColorPalette';
+import NewPaletteForm from './NewPaletteForm';
 
 function findPalette(id) {
   return seedColors.find(function (palette) {
@@ -16,6 +17,7 @@ function App() {
   return (
     <Switch>
       <Route exact path="/" render={routeProps => (<PaletteList palettes={seedColors} {...routeProps} />)} />
+      <Route exact path="/palette/new" render={() => (<NewPaletteForm />)} />
       <Route
         exact path="/palette/:id"
         render={routeProps => <Palette palette={generatePalette(findPalette(routeProps.match.params.id))} />}
