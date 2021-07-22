7a4838a8be1a6286221898d15cb6afc4aded63bd
Derek Widmer <dwidmer99@gmail.com>
2021-07-21 20:31:30 -0400

    Finished refactoring ColorBox css to jsx

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -58,6 +58,68 @@ const styles = {
         textDecoration: "none",
         color: props => chroma(props.background).luminance() >= 0.6 ? 'rgba(0,0,0,0.7)' : 'white',
         opacity: "0"
+    },
+    boxContent: {
+        position: "absolute",
+        padding: "10px",
+        width: "90%",
+        left: "0",
+        bottom: "0",
+        letterSpacing: "1px",
+        textTransform: "uppercase",
+        fontSize: "12px",
+        color: "black",
+    },
+    copyOverlay: {
+        opacity: "0",
+        zIndex: "0",
+        width: "100%",
+        height: "100%",
+        transition: "transform 0.5s ease-in-out",
+        transform: "scale(0.1)",
+    },
+    showOverlay: {
+        opacity: "1",
+        transform: "scale(50)",
+        zIndex: "10",
+        position: "absolute",
+    },
+    copyMsg: {
+        position: "fixed",
+        left: "0",
+        right: "0",
+        top: "0",
+        bottom: "0",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "4rem",
+        transform: "scale(0.001)",
+        opacity: "0",
+        color: "white",
+        flexDirection: "column",
+    },
+    showMessage: {
+        opacity: "1",
+        transform: "scale(1)",
+        zIndex: "25",
+        transition: "all 0.4s ease-in-out",
+        transitionDelay: "0.3s",
+        "& h1": {
+            fontWeight: "400",
+            textShadow: "1px 2px black",
+            background: "rgba(255,255,255,0.2)",
+            width: "100%",
+            textAlign: "center",
+            marginBottom: "0",
+            padding: "1rem",
+            textTransform: "uppercase",
+            fontSize: "4rem",
+        },
+        "& p": {
+            fontSize: "2rem",
+            fontWeight: "100",
+        }
     }
 }
 
@@ -77,19 +139,19 @@ class ColorBox extends Component {
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div className={classes.ColorBox} style={{ background }}>
-                    <div className={`copy-overlay ${this.state.copied ? 'show' : ''}`} style={{ background }} />
-                    <div className={`copy-msg ${this.state.copied ? 'show' : ''}`}>
+                    <div className={`${classes.copyOverlay} ${this.state.copied && classes.showOverlay}`} style={{ background }} />
+                    <div className={`${classes.copyMsg} ${this.state.copied && classes.showMessage}`}>
                         <h1>Copied!</h1>
                         <p className={classes.copyText}>{background}</p>
                     </div>
-                    <div className="copy-container">
-                        <div className="box-content">
+                    <div>
+                        <div className={classes.boxContent}>
                             <span className={classes.colorName}>{name}</span>
                         </div>
                         <button className={classes.copyButton}>Copy</button>
                     </div>
                     {showingFullPalette && (
-                        < Link to={moreUrl} onClick={e => e.stopPropagation()}>
+                        <Link to={moreUrl} onClick={e => e.stopPropagation()}>
                             <span className={classes.seeMore}>MORE</span>
                         </Link>
                     )}
