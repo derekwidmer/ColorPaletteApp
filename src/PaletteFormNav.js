aa85b6e3482505a4dbcadf087ac6944e146198ff
Derek Widmer <dwidmer99@gmail.com>
2021-07-29 17:12:18 -0400

    Added save modal and used class component instead of functional

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import clsx from 'clsx';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import PaletteMetaForm from "./PaletteMetaForm";
+import { Link } from "react-router-dom";
+import { withStyles } from '@material-ui/core/styles';
+import classNames from 'classnames';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import Button from '@material-ui/core/Button'
-import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator'
-import PaletteMetaForm from './PaletteMetaForm';
+import { Button } from "@material-ui/core";
 
-const drawerWidth = 450;
-const useStyles = makeStyles((theme) => ({
+const drawerWidth = 400;
+
+const styles = theme => ({
     root: {
-        display: "flex"
+        display: "flex",
     },
     appBar: {
         transition: theme.transitions.create(['margin', 'width'], {
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
-        height: "64px"
+        height: "64px",
     },
     appBarShift: {
         width: `calc(100% - ${drawerWidth}px)`,
@@ -36,65 +36,90 @@ const useStyles = makeStyles((theme) => ({
         }),
     },
     menuButton: {
-        marginRight: theme.spacing(2),
+        marginLeft: 12,
+        marginRight: 20,
     },
-    navButtons: {
+    navBtns: {
         marginRight: "1rem",
-        alignItems: "center"
+        "& a": {
+            textDecoration: "none",
+        },
     },
-    button: {
+    buttonNav: {
         margin: "0 0.5rem",
     },
-    link: {
-        textDecoration: "none"
+});
+
+class PaletteFormNav extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newPaletteName: "",
+            formShowing: false,
+        }
+        this.showForm = this.showForm.bind(this);
+        this.hideForm = this.hideForm.bind(this);
     }
-}));
 
-export default function PaletteFormNav({ open, handleSubmit, palettes, handleDrawerOpen }) {
-    const classes = useStyles();
-    const [formShowing, updateFormShowing] = React.useState(false)
-
-    const showForm = () => {
-        updateFormShowing(!formShowing)
+    showForm() {
+        this.setState({ formShowing: true });
     }
 
-    return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <AppBar
-                position="fixed"
-                color="default"
-                className={clsx(classes.appBar, {
-                    [classes.appBarShift]: open,
-                })}
-            >
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        edge="start"
-                        className={clsx(classes.menuButton, open && classes.hide)}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" noWrap>
-                        Create a Palette
-                    </Typography>
-                </Toolbar>
-                <div className={classes.navButtons}>
-                    <Button
-                        className={classes.button}
-                        variant="contained"
-                        color="primary"
-                        onClick={showForm}
-                    >
-                        Save Palette
-                    </Button>
-                    <Link to="/" className={classes.link}><Button variant="contained" color="secondary" className={classes.button}>Go Back</Button></Link>
-                </div>
-            </AppBar>
-            {!formShowing && <PaletteMetaForm palettes={palettes} handleSubmit={handleSubmit} />}
-        </div>
-    )
+    hideForm() {
+        this.setState({ formShowing: false });
+    }
+
+    render() {
+        const { classes, open, palettes, handleSubmit } = this.props;
+        return (
+            <div className={classes.root}>
+                <CssBaseline />
+                <AppBar
+                    position="fixed"
+                    color='default'
+                    className={classNames(classes.appBar, {
+                        [classes.appBarShift]: open,
+                    })}
+                >
+                    <Toolbar disableGutters={!open}>
+                        <IconButton
+                            color="inherit"
+                            aria-label="Open drawer"
+                            onClick={this.props.handleDrawerOpen}
+                            className={classNames(classes.menuButton, open && classes.hide)}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" noWrap>
+                            Create a Palette
+                        </Typography>
+                    </Toolbar>
+                    <div className={classes.navBtns}>
+                        <Link to="/">
+                            <Button variant="contained" color="secondary" className={classes.buttonNav}>
+                                Go Back
+                            </Button>
+                        </Link>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={this.showForm}
+                            className={classes.buttonNav}
+                        >
+                            Save
+                        </Button>
+                        {this.state.formShowing && (
+                            <PaletteMetaForm
+                                palettes={palettes}
+                                handleSubmit={handleSubmit}
+                                hideForm={this.hideForm}
+                            />
+                        )}
+                    </div>
+                </AppBar>
+            </div>
+        );
+    }
 }
+
+export default withStyles(styles, { withTheme: true })(PaletteFormNav);
--- src/PaletteMetaForm.js
+++ src/PaletteMetaForm.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -8,70 +7,72 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 
-export default function PaletteMetaForm({ palettes, handleSubmit }) {
-    const [open, setOpen] = React.useState(true);
-    const [newPaletteName, setPaletteName] = React.useState("")
+class PaletteMetaForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: true,
+            newPaletteName: "",
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-    const handleClickOpen = () => {
-        setOpen(true);
+    componentDidMount() {
+        ValidatorForm.addValidationRule('isPaletteNameUnique', value =>
+            this.props.palettes.every(
+                ({ paletteName }) => paletteName.toLowerCase() !== value.toLowerCase()
+            )
+        );
+    }
+
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        });
+    }
+
+    handleClickOpen = () => {
+        this.setState({ open: true });
     };
 
-    const handleClose = () => {
-        setOpen(false);
+    handleClose = () => {
+        this.setState({ open: false });
     };
 
-    React.useEffect(() =>
-        ValidatorForm.addValidationRule("isPaletteNameUnique", value => {
-            return palettes.every(
-                ({ paletteName }) =>
-                    paletteName.toLowerCase() !== newPaletteName.toLowerCase()
-            );
-        })
-    );
-
-    return (
-        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-            <DialogTitle id="form-dialog-title">Name Your Palette</DialogTitle>
-            <ValidatorForm onSubmit={() => handleSubmit(newPaletteName)}>
-
-                <DialogContent>
-                    <DialogContentText>
-                        Please enter a name for your new palette. Make sure it is unique!
-                    </DialogContentText>
-                    <TextValidator
-                        label="Palette Name"
-                        value={newPaletteName}
-                        name="newPaletteName"
-                        onChange={evt => { setPaletteName(evt.target.value) }}
-                        validators={['required', 'isPaletteNameUnique']}
-                        errorMessages={['Palette name required', 'Palette name must be unique']}
-                        fullWidth
-                        margin="normal"
-                    />
-
-
-                    {/* <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Email Address"
-                        type="email"
-                        fullWidth
-                    /> */}
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary">
-                        Cancel
-                    </Button>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        type="submit"
-                    >
-                        Save
-                    </Button>
-                </DialogActions>
-            </ValidatorForm>
-        </Dialog>
-    );
+    render() {
+        const { newPaletteName } = this.state;
+        const { hideForm, handleSubmit } = this.props;
+        return (
+            <Dialog open={this.state.open} onClose={hideForm} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Choose a Palette Name</DialogTitle>
+                <ValidatorForm onSubmit={() => handleSubmit(newPaletteName)}>
+                    <DialogContent>
+                        <DialogContentText>
+                            Please enter a name for your new beautiful Palette. Make sure it's unique.
+                        </DialogContentText>
+                        <TextValidator
+                            label="Palette Name"
+                            name="newPaletteName"
+                            value={newPaletteName}
+                            onChange={this.handleChange}
+                            fullWidth
+                            margin="normal"
+                            validators={["required", "isPaletteNameUnique"]}
+                            errorMessages={["Enter Palette Name", "Name already used"]}
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={hideForm} color="primary">
+                            Cancel
+                        </Button>
+                        <Button variant="contained" color="primary" type="submit">
+                            Save Palette
+                        </Button>
+                    </DialogActions>
+                </ValidatorForm>
+            </Dialog>
+        );
+    }
 }
+
+export default PaletteMetaForm;
